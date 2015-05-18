495cb34adc16289411d026a5ba2ffb60c21255af
Michael Hadley <michaelwesthadley@gmail.com>
2015-05-17 21:40:29 -0500

    Updating jQuery API example

--- Projects/NutrientCalculator/jQueryAccessingAPI/main.js
+++ Projects/NutrientCalculator/jQueryAccessingAPI/main.js
@@ -1,7 +1,7 @@
 // Code wrapped in annonymous function to prevent collisions
-(function() {
+$(document).ready(function() {
 
-  var ndbAPICall = "http://api.nal.usda.gov/usda/ndb/search/";
+  var ndbAPICall = "http://api.nal.usda.gov/usda/ndb/search";
   var callParameters = {
     format: "json",
     q: "butter",
@@ -11,10 +11,16 @@
     api_key: "nGaK10u7f4V6pugwf4goIeJtFo6fLKSBKjt4YlI8"
   };
 
-  function handler(data) {
+  function handler(data, status) {
+    $("h2#requestStatus").text("Status of the search request: " + status);
+    $("h2#version").text("NDB Version: " + data.list.sr);
+    $("h2#query").text("Query: " + data.list.q);
+    for (var i=0; i<data.list.item.length; i++) {
+      $("ul#searchResults").append("<li>" + data.list.item[i].name + "</li>");
+    }
     console.log("Recieved data");
   }
 
   $.getJSON(ndbAPICall, callParameters, handler);
 
-})();
+});
